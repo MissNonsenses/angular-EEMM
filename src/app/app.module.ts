f486bd2d25b05540d931ef839d59286e9e8e8fce
MissNonsenses <ofbregistros@gmail.com>
2020-10-14 12:14:17 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatCardModule } from '@angular/material/card'; 
-import { MatInputModule } from '@angular/material/input'; 
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { MatSliderModule } from "@angular/material/slider";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
-  imports:      
-    [ BrowserModule, FormsModule, 
-    MatSliderModule, MatCardModule, MatInputModule, 
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatInputModule,
     MatGridListModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+    MatButtonModule
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
