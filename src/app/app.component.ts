217afe9bff7a2e283d4750109358586b2c834f96
MissNonsenses <ofbregistros@gmail.com>
2020-10-15 15:06:51 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -9,50 +8,65 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class AppComponent  {
    name = 'EEMM ';
+ options: FormGroup;
+  colorControl = new FormControl('primary');
+  fontSizeControl = new FormControl(16, Validators.min(10));
 
-form;
-  editUnit = {
-    UnitName: 'Unit Name',
-    Area: 'Area',
-    OwnerId: 'Owner Id',
-    IsWithParking: 'IsWithParking',
-    ParkingArea: 'Parking Area'
-  };
-
-  yesNo = [
-    { value: 'Yes', name: 'YES' },
-    { value: 'No', name: 'NO' },
-  ];
-
-  persons = [
-    { PersonId: 'Person Id', FirstName: 'First Name', MiddleName: 'MiddleName', LastName: 'Last Name' },
-  ];
-
-  get UnitName() {
-    return this.form.get('UnitName');
-  }
-
-  get OwnerId() {
-    return this.form.get('OwnerId');
-  }
-
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.form = this.fb.group({
-      UnitName: ['this.editUnit.UnitName', Validators.required],
-      Area:     ['this.editUnit.Area', Validators.required],
-      OwnerId: ['this.editUnit.OwnerId'],
-      IsWithParking: ['this.editUnit.IsWithParking'],
-      ParkingArea: ['this.editUnit.ParkingArea'],
+  constructor(fb: FormBuilder) {
+    this.options = fb.group({
+      color: this.colorControl,
+     
+      fontSize: this.fontSizeControl,
     });
   }
 
-  unitNameError() {}
-  areaError() {}
-  ownerIdError() {}
-  updateUnit() {}
-  cancel() {}
+   getFontSize() {
+     return Math.max(10, this.fontSizeControl.value);
+   }
+
+//form;
+  // editUnit = {
+  //   UnitName: 'Unit Name',
+  //   Area: 'Area',
+  //   OwnerId: 'Owner Id',
+  //   IsWithParking: 'IsWithParking',
+  //   ParkingArea: 'Parking Area'
+  // };
+
+  // yesNo = [
+  //   { value: 'Yes', name: 'YES' },
+  //   { value: 'No', name: 'NO' },
+  // ];
+
+  // persons = [
+  //   { PersonId: 'Person Id', FirstName: 'First Name', MiddleName: 'MiddleName', LastName: 'Last Name' },
+  // ];
+
+  // get UnitName() {
+  //   return this.form.get('UnitName');
+  // }
+
+  // get OwnerId() {
+  //   return this.form.get('OwnerId');
+  // }
+
+  // constructor(private fb: FormBuilder) {}
+
+  // ngOnInit() {
+  //   this.form = this.fb.group({
+  //     UnitName: ['this.editUnit.UnitName', Validators.required],
+  //     Area:     ['this.editUnit.Area', Validators.required],
+  //     OwnerId: ['this.editUnit.OwnerId'],
+  //     IsWithParking: ['this.editUnit.IsWithParking'],
+  //     ParkingArea: ['this.editUnit.ParkingArea'],
+  //   });
+  // }
+
+  // unitNameError() {}
+  // areaError() {}
+  // ownerIdError() {}
+  // updateUnit() {}
+  // cancel() {}
 
 
 
