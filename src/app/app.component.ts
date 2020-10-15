bffaf6e3333fbf03d41efbdde22b614bef1b6dbc
MissNonsenses <ofbregistros@gmail.com>
2020-10-15 14:24:42 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
@@ -10,7 +10,49 @@ import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from "@angular/materia
 export class AppComponent  {
    name = 'EEMM ';
 
+form;
+  editUnit = {
+    UnitName: 'Unit Name',
+    Area: 'Area',
+    OwnerId: 'Owner Id',
+    IsWithParking: 'IsWithParking',
+    ParkingArea: 'Parking Area'
+  };
 
+  yesNo = [
+    { value: 'Yes', name: 'YES' },
+    { value: 'No', name: 'NO' },
+  ];
+
+  persons = [
+    { PersonId: 'Person Id', FirstName: 'First Name', MiddleName: 'MiddleName', LastName: 'Last Name' },
+  ];
+
+  get UnitName() {
+    return this.form.get('UnitName');
+  }
+
+  get OwnerId() {
+    return this.form.get('OwnerId');
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      UnitName: ['this.editUnit.UnitName', Validators.required],
+      Area:     ['this.editUnit.Area', Validators.required],
+      OwnerId: ['this.editUnit.OwnerId'],
+      IsWithParking: ['this.editUnit.IsWithParking'],
+      ParkingArea: ['this.editUnit.ParkingArea'],
+    });
+  }
+
+  unitNameError() {}
+  areaError() {}
+  ownerIdError() {}
+  updateUnit() {}
+  cancel() {}
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
-import { MatSliderModule } from "@angular/material/slider";
+
 import { MatCardModule } from "@angular/material/card";
 import { MatInputModule } from "@angular/material/input";
 import { MatGridListModule } from "@angular/material/grid-list";
@@ -15,8 +16,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog'; 
 import { MatSelectModule } from '@angular/material/select';
 
+
 @NgModule({
-  imports: [ BrowserModule, FormsModule, MatSliderModule, MatCardModule, MatInputModule, MatGridListModule, BrowserAnimationsModule,MatToolbarModule, MatButtonModule, FlexLayoutModule, MatFormFieldModule,MatDialogModule,MatSelectModule, ],
+  imports: [ ReactiveFormsModule, BrowserModule, FormsModule, MatCardModule, MatInputModule, MatGridListModule, BrowserAnimationsModule,MatToolbarModule, MatButtonModule, FlexLayoutModule, MatFormFieldModule,MatDialogModule,MatSelectModule, ],
   declarations: [AppComponent, HelloComponent, ],
   bootstrap: [AppComponent]
 })
